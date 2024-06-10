00ea5b85aa30b2d989b3e1cdd46c4acbddf88773
Lucas Luize <lucasluize.tech@gmail.com>
2024-06-10 13:00:02 -0400

    #1: First Files, textnode.py added , server.py added

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    text_node = TextNode("Hello World", "bold", "https://www.example.com")
+    print(text_node)
+
+if __name__ == "__main__":
+    main()
